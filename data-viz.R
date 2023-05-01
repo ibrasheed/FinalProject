aea94236fcd7233d64fd2edc1896ec127457d2d1
ibrasheed <123114598+ibrasheed@users.noreply.github.com>
2023-05-01 13:50:25 -0400

    Update data viz

--- data-viz.R
+++ data-viz.R
@@ -3,9 +3,8 @@ library(dplyr)
 library(wbstats)
 library(RColorBrewer)
 library(sf)
-library(gganimate)
-library(gapminder)
-library("cowplot") 
+library(fields)
+
 
 
 setwd("/Users/rasheed/Documents/GitHub/FinalProject")
@@ -19,8 +18,25 @@ country_map <- st_read("afshape/Africa_Boundaries.shp")
 ## Merging shapefile with corruption data
 df_map <- merge(country_map, corruption, by.x= "ISO", by.y = "iso3c", all.x = T)
 names(df_map)[names(df_map) == 'CC.EST'] <- 'Control'
+df_map$Controln <- df_map$Control + 2.5
+plot(as.data.frame(df_map), col = df_map$control)
 
-##Plotting Data ----
+my_palette <- colorRampPalette(c("red", "green"))
+pdf(file = "controlmap-baseR.pdf")
+plot(df_map$geometry, col = my_palette(50)[cut(df_map$Controln, breaks = 50)], max.plot = 13) 
+legend("topright", 
+       legend = c("Low", "High"),
+       fill = my_palette(2),
+       title = "Control of Corruption",
+       box.lty = 0)
+
+dev.off()
+
+
+#~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~
+
+
+##Plotting geospatial data with ggplot----
 
 color_palette <- colorRampPalette(brewer.pal(6, "RdYlBu"))
 pdf(file = "corruptionmap3.pdf")
@@ -34,23 +50,25 @@ dev.off()
 #~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~
 
 
-# plotting corruption control by wave and country-------------------------------------------------------------------------------
+
+# plotting corruption control by wave and country-----
 
 data <- read.csv("forAnalysis.csv")
-pdf(file = "Country-wave.pdf")
 
-ggplot(data, aes(x = country, y = control.cor, group = wave, color = wave)) +
+pdf(file = "Country-wave1.pdf", paper = "a4r", width = 10 )
+
+ggplot(data, aes(x = iso3c, y = control.cor, group = wave, color = wave)) +
   geom_point(aes(shape = wave), size=2, alpha = .5) +
   labs(title = "Control of Corruption by Country and Wave",
        x = "Country", y = "Control of Corruption") +
-  theme( legend.position = c(.95, .9)) +
-  scale_color_manual(values = c( "#DC143C", "#0000FF","#00FF7F")) +
+  theme( legend.position = c(.95, .9), axis.text=element_text(size=7)) +
+  scale_color_manual(values = c( "#DC143C", "#0000FF","#228B22")) +
   coord_flip()
 
-
-
-
 dev.off()
+
+
+#~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~
 
 
 
